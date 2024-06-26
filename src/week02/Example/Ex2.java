27cfba06ad6c3e3075129b3e292be25023a7935c
gminnimk <tvt1245@naver.com>
2024-06-26 15:31:08 +0900

    JAVA 2주차 - 다차원 배열 조회 예제

---
+++ src/week02/Example/Ex2.java
@@ -0,0 +1,20 @@
+package week02.Example;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        //다차원 배열 조회
+        // 최대값 구하기 & 최소값 구하기
+
+        // 최대값 구하기
+
+        int[] arr = {3, 2, 1, 5, 1};
+
+        int max = arr[0];
+
+        // 최대값 구하기
+        for (int num : arr) {
+            if (num > max) { // 반복문 돌면서 나(max)보다 값이 작으면 저장
+            }
+        }
+    }
+}
