f383f66fc6a3e728430451e567a885632f21cc91
gminnimk <tvt1245@naver.com>
2024-06-26 15:31:23 +0900

    JAVA2 - 구구단 예제

---
+++ src/week02/Example/Ex1.java
@@ -0,0 +1,21 @@
+package week02.Example;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = { 1, 2, 3, 4 };
+        int[] b = a.clone();
+
+
+//        // 구구단 생성기
+
+
+        // 구구단에서 조건문을 통해 일부 단 출력만 제외해보기!
+
+
+    }
+}
